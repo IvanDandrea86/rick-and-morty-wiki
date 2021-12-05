7f03b53e55a9054d523e85fc042f4b68d9a67f57
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-12-05 13:19:24 +0100

    Pagination and Serachbar added

--- src/App.js
+++ src/App.js
@@ -9,33 +9,45 @@ import Pagination from "./components/Pagination/Pagination";
 import Filter from "./components/Filter/Filter";
 import Navbar from "./components/Navbar/Navbar";
 
-let api = `https://rickandmortyapi.com/api/character/?page=1`
 
 
 
 
-function App() {
+
+const App=()=> {
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+  
   useEffect(() => {
     (async function () {
       let data = await fetch(api).then((res) => res.json());
       updateFetchedData(data);
     })();
   }, [api]);
-  let [fetchedData, updateFetchedData] = useState([]);
-  let { info, results } = fetchedData;
+  
   return (
     <div className="App">
     <h1 className="text-center mb-3">Characters</h1>
+    <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
     <div className="container">
     <div className="row">
       Filter component will be placed here
       <div className="col-lg-8 col-12">
         <div className="row">
-          Card component will be placed here
+        <Card results={results} />
         </div>
       </div>
     </div>
     </div>
+    <Pagination
+  info={info}
+  pageNumber={pageNumber}
+  updatePageNumber={updatePageNumber}
+/>
   </div>
   );
 }
